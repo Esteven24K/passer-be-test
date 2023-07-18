f7eceb2619fadf241608f36cc0f311d4e5dd33f3
Esteven24k <stevenatencio334@gmail.com>
2023-07-17 23:26:53 -0600

    TS-03

--- databases/postgresql.js
+++ databases/postgresql.js
@@ -6,6 +6,10 @@ const postgresql = newDb();
 postgresql.public.none(`create table users(pk_user integer, name text, status boolean);
                 insert into users values (123, 'Juan', true);`);
 
+postgresql.public.none(`create table transaction(pk_transaction integer, fk_user integer, description text, amount float);
+    insert into transaction (pk_transaction, fk_user, description, amount)
+    values (1, 123, 'Descripción de la transacción', 10.5);`);
+
 
 module.exports = {
     postgresql
